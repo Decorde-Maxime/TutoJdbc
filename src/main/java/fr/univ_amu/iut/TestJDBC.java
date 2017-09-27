99bd86b624eec91b98714df1d0c21be4fb0a6b57
Maxime-Decorde <maxime.decorde@etu.univ-amu.fr>
2017-09-27 14:11:39 +0200

    Donnees persos completees

--- src/main/java/fr/univ_amu/iut/TestJDBC.java
+++ src/main/java/fr/univ_amu/iut/TestJDBC.java
@@ -5,9 +5,9 @@ import java.sql.*;
 
 public class TestJDBC {
 	// Chaine de connexion
-	static final String CONNECT_URL = "jdbc:mysql://localhost:3306/gestionPedaBD";
-	static final String LOGIN = "monUser";
-	static final String PASSWORD = "monPassword";
+	static final String CONNECT_URL = "jdbc:mysql://mysql-maxdecorde.alwaysdata.net:3306/maxdecorde_jdbc";
+	static final String LOGIN = "144529_maxdecord";
+	static final String PASSWORD = "Maxime131";
 	// La requete de test
 	static final String req = "SELECT NUM_ET, NOM_ET, PRENOM_ET " +
 	                          "FROM ETUDIANT " +
